6a6edc3b6deb890cd236e8f5d85d014821c4e8c3
Addisu <addisuhaile87@gmail.com>
2025-01-15 11:14:35 +0300

    Adding an api route to like_post

--- blogapi/routers/post.py
+++ blogapi/routers/post.py
@@ -4,10 +4,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from blogapi.core.deps import get_current_user
-from blogapi.database.database import comment_table, database, post_table
+from blogapi.database.database import comment_table, database, like_table, post_table
 from blogapi.models.post import (
     Comment,
     CommentIn,
+    PostLike,
+    PostLikeIn,
     UserPost,
     UserPostIn,
     UserPostWithComments,
@@ -109,3 +111,23 @@ async def get_post_with_comments(post_id: int):
         "post": post,
         "comments": await get_comments_on_post(post_id),
     }
+
+
+@router.post("/like", response_model=PostLike, status_code=201)
+async def like_post(
+    like: PostLikeIn, current_user: Annotated[User, Depends(get_current_user)]
+):
+    logger.info("Liking post")
+
+    post = await find_post(like.post_id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found."
+        )
+    data = {**like.model_dump(), "user_id": current_user.id}
+    query = like_table.insert().values(data)
+
+    logger.debug(query)
+
+    last_record_id = await database.execute(query)
+    return {"id": last_record_id, **data}
