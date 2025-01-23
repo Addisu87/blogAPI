ef1ce6da2eddbc761bd99c52e70f58064bd84c2f
Addisu <addisuhaile87@gmail.com>
2025-01-23 12:42:11 +0300

    Fix error for santry

--- blogapi/main.py
+++ blogapi/main.py
@@ -13,21 +13,18 @@ from blogapi.routers.post import router as post_router
 from blogapi.routers.upload import router as upload_router
 from blogapi.routers.user import router as user_router
 
-logger = logging.getLogger(__name__)
-
-
 sentry_sdk.init(
     dsn=config.SENTRY_DSN,
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
-    _experiments={
-        # Set continuous_profiling_auto_start to True
-        # to automatically start the profiler on when
-        # possible.
-        "continuous_profiling_auto_start": True,
-    },
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
 )
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -44,11 +41,6 @@ app.add_middleware(CorrelationIdMiddleware)
 app.include_router(post_router)
 app.include_router(upload_router)
 app.include_router(user_router)
-
-
-@app.get("/sentry-debug")
-async def trigger_error():
-    division_by_zero = 1 / 0
 
 
 @app.exception_handler(HTTPException)
