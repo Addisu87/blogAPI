0c8b0a3f423c285fdcb105bdcd1b6e4f3bc085eb
Addisu <addisuhaile87@gmail.com>
2025-01-23 12:26:08 +0300

    Sentry add to blogapi

--- blogapi/main.py
+++ blogapi/main.py
@@ -1,10 +1,12 @@
 import logging
 from contextlib import asynccontextmanager
 
+import sentry_sdk
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, HTTPException
 from fastapi.exception_handlers import http_exception_handler
 
+from blogapi.core.config import config
 from blogapi.core.logging_conf import configure_logging
 from blogapi.database.database import database
 from blogapi.routers.post import router as post_router
@@ -12,6 +14,20 @@ from blogapi.routers.upload import router as upload_router
 from blogapi.routers.user import router as user_router
 
 logger = logging.getLogger(__name__)
+
+
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
 
 
 @asynccontextmanager
@@ -28,6 +44,11 @@ app.add_middleware(CorrelationIdMiddleware)
 app.include_router(post_router)
 app.include_router(upload_router)
 app.include_router(user_router)
+
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
 
 
 @app.exception_handler(HTTPException)
